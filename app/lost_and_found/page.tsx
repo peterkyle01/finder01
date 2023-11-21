8a3d66b59651aeced9b8380f8cb6f6f71fff41c3
peterkyle01 <kylepeterkoine4@gmail.com>
2023-11-21 18:16:48 +0300

    commit v2

--- app/lost_and_found/page.tsx
+++ app/lost_and_found/page.tsx
@@ -1,5 +1,49 @@
-export default function page() {
-  return (
-    <div>lost and found</div>
-  )
+import { foundIDType, lostIDType } from "@/types";
+import LostAndFoundTable from "./LostAndFoundTable";
+
+async function getLostIDs() {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/lost`, {
+		next: { revalidate: 0 },
+	});
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch lost IDs!");
+	}
+
+	return res.json();
+}
+
+async function getFoundIDs() {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/found`, {
+		next: { revalidate: 0 },
+	});
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch found IDs!");
+	}
+
+	return res.json();
+}
+
+async function getLostAndFoundIDs() {
+	const lostIDs: lostIDType[] = await getLostIDs();
+	const foundIDs: foundIDType[] = await getFoundIDs();
+
+	const _lostAndFoundIDs = lostIDs.filter((lostID) => {
+		return foundIDs.some((foundID) => {
+			return lostID.student_adm === foundID.student_adm;
+		});
+	});
+
+	return _lostAndFoundIDs;
+}
+
+export default async function LostAndFound() {
+	const lostAndFoundIDs = await getLostAndFoundIDs();
+	console.log(lostAndFoundIDs);
+	return (
+		<div className="w-full h-[100dvh]">
+			<LostAndFoundTable lostIDs={lostAndFoundIDs} />
+		</div>
+	);
 }
